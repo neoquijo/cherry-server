43f5a5125f998a28b21fb102081ef57348c8cfcb
neoquijo <neoquijo@pm.me>
2024-01-21 04:39:35 +0100

    organization dependency

--- src/pointsOfSale/pointsOfSale.module.ts
+++ src/pointsOfSale/pointsOfSale.module.ts
@@ -7,19 +7,23 @@ import { ImageHandlerModule } from 'src/image-handler/image-handler.module';
 import { PointsOfSaleService } from './pointsOfSale.service';
 import { BusinessOwnersModule } from 'src/businessOwners/businessOwners.module';
 import { PosAdress, PosAdressSchema } from './models/posadress.schema';
+import { OrganizationsModule } from 'src/organizations/organizations.module';
+import { Organizations, OrganizationsSchema } from 'src/organizations/models/organization.schema';
 
 @Module({
   providers: [PointsOfSaleService],
   controllers: [PointsOfSaleAdminController],
   imports: [
     AuthModule,
+    OrganizationsModule,
     ImageHandlerModule,
     BusinessOwnersModule,
     MongooseModule.forFeature([
+      { name: Organizations.name, schema: OrganizationsSchema },
       { name: POS.name, schema: POSSchema },
       { name: PosAdress.name, schema: PosAdressSchema },
     ]),
   ],
   exports: [PointsOfSaleService],
 })
-export class PointsOfSaleModule {}
+export class PointsOfSaleModule { }
